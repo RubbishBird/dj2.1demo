ef77f1a3ad7ecce4434bbbb2264e2f87c2939988
RubbishBird <1158784496@qq.com>
2018-09-25 19:51:35 +0800

    dj2.1demo

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -1,14 +1,20 @@
 '''为应用程序users定义url模式'''
 
 from django.urls import path,include
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView,LogoutView
 from . import views
+from atexit import register
 
 #修改模板路径
 LoginView.template_name = 'users/login.html'
+
 
 app_name='[users]'
 urlpatterns = [
     #登录页面
     path('login/',LoginView.as_view(),name='login'),
+    #注销
+    path('logout/',LogoutView.as_view(),name='logout'),
+    #注册页面
+    path('register/',register,name='register')
 ]
